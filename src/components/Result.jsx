db83857cf9f4eef8033c8f6b38a5af88e074c545
Steve Tyler <steve.tyler@centrica.com>
2024-04-26 18:13:49 +0100

    update

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -4,8 +4,12 @@ import { formatter } from "../util/investment";
 export default function Result({ userValues }) {
 
   const resultsArr = calculateInvestmentResults(userValues);
-  console.log(resultsArr);
-  
+  //console.log(resultsArr);
+  const initialInvestment = 
+    resultsArr[0].valueEndOfYear - 
+    resultsArr[0].interest - 
+    resultsArr[0].annualInvestment; 
+
   return (
     <table id="result">
       <thead>
@@ -18,14 +22,20 @@ export default function Result({ userValues }) {
         </tr>
       </thead>
       <tbody>
-      {resultsArr.map((resultObj, i, array) => {
+      {resultsArr.map((yearData, i, array) => {
+        const totalInterest = 
+          yearData.valueEndOfYear - 
+          yearData.annualInvestment * yearData.year -
+          initialInvestment; 
+        const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
+
         return (
           <tr>
-            <td>{resultObj.year}</td>
-            <td>{formatter.format(resultObj.valueEndOfYear)}</td>
-            <td>{formatter.format(resultObj.interest)}</td>
-            
-            <td></td>
+            <td>{yearData.year}</td>
+            <td>{formatter.format(yearData.valueEndOfYear)}</td>
+            <td>{formatter.format(yearData.interest)}</td>
+            <td>{formatter.format(totalInterest)}</td>
+            <td>{formatter.format(totalAmountInvested)}</td>
           </tr>
         )
       })}
